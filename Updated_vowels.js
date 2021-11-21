b7e60372dd352cb26d8aef8038aa6e2ef5d3db7f
Reza Jaffery <raze77@hotmail.com>
2021-11-21 23:01:45 +0000

    Final version

--- Updated_vowels.js
+++ Updated_vowels.js
@@ -1,21 +1,21 @@
-
-
 const vowels = ["a", "e", "i", "o", "u"];
 
-const string = prompt('Please enter your message: ');
-
-let editedString = string.toLowerCase();
-let vowelNumber = 0
-const countVowels = () => {
-    for (let i = 0; i < editedString.length; i++){
+const countVowels = (str) => {
+  let vowelNumber = 0
+    for (let i = 0; i < str.length; i++){
 
         for (let v = 0; v < vowels.length; v++){
 
-          if (editedString[i] === vowels[v]){
+          if (str[i] === vowels[v]){
               vowelNumber++
           }
         }
         }
  return vowelNumber
     }
-    console.log(`The number of vowels is ${countVowels(string)}`);
+
+    const string = prompt('Please enter your message: ');
+
+    let editedString = string.toLowerCase();
+
+    console.log(`The number of vowels is ${countVowels(editedString)}`);
