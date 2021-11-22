aca346f7aae75e3632380c0db623f7b596a5e3ab
Reza Jaffery <raze77@hotmail.com>
2021-11-22 00:46:23 +0000

    Corrected counter

--- Updated_lottery.js
+++ Updated_lottery.js
@@ -3,7 +3,7 @@ let generateNumber = 0
 
 const getLottery = (amount, range) => {
 
-for (let counter = 1; counter <= range; counter++) {
+for (let counter = 0; counter < range; counter++) {
   generateNumber = Math.floor(Math.random() * amount) + 1;
   lotteryNumbers.push(generateNumber);
 }
@@ -14,7 +14,7 @@ for (let counter = 1; counter <= range; counter++) {
     for (let counter = 0; counter < range; counter++) {
        console.log(`Lottery number ${counter + 1} is ${lotteryNumbers[counter]}`);
    }
- } 
+ }
 
 let value = prompt('Please enter the maximum value of lottery number to calculate');
 let numbers = prompt('PLease specify how many lottery numbers to calculate ');
@@ -22,5 +22,3 @@ let numbers = prompt('PLease specify how many lottery numbers to calculate ');
 getLottery(value, numbers);
 
 printLottery(lotteryNumbers.length);
-
-
